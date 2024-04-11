6de2db95df7d35b8c19f002bb37c2ecf2aaac2ef
ange.shum <angel.shum@2t1l.co>
2024-04-11 19:46:42 +0800

    first commit

---
+++ expo-updates-client/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Updating 5</Text>
+      <Text>{Constants.expoConfig.name}</Text>
+      <Image source={require('./assets/favicon.png')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
