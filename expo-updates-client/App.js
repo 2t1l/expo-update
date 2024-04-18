671328a14f779b7d58ab8e60bc0783a74c3b81c9
ange.shum <angel.shum@2t1l.co>
2024-04-18 14:09:03 +0800

    chage url in expo config

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -1,8 +1,28 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Constants from 'expo-constants';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Updating from production 5 vercel 3</Text>
